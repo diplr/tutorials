8f31547e148512552b5c90c5caf20aa6e778f931
Quinten De Swaef <quintendeswaef@gmail.com>
2019-03-13 14:42:16 +0100

    Quarkus Async Example

--- quarkus/quarkus-getting-started-async/src/main/java/org/diplr/quarkus/core/controller/GreeterController.java
+++ quarkus/quarkus-getting-started-async/src/main/java/org/diplr/quarkus/core/controller/GreeterController.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.concurrent.CompletableFuture;
 
 @Path("/greeting")
 public class GreeterController {
@@ -16,7 +17,7 @@ public class GreeterController {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return greeterService.greet("world");
+    public CompletableFuture<String> hello() {
+        return CompletableFuture.supplyAsync(() -> greeterService.greet("world async"));
     }
 }
